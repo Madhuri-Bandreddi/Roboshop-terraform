1911da43bdbbb35a48351141b69a2225c6ae5a5b
mammu <bandreddymammu599@gmail.com>
2025-02-15 22:14:52 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -5,7 +5,7 @@ module "servers" {
   component_name = each.value["name"]
   env            = var.env
   instance_type  = each.value["instance_type"]
-  password = lookup(each.value, "password", "null")
+  password       = lookup(each.value, "password", "null")
 }
 
 
@@ -98,6 +98,6 @@ resource "aws_route53_record" "records" {
   name           = "${each.value["name"]}.madhari123.shop"
   type           = "A"
   ttl            = 30
-  records        = [aws_instance.instance[each.value].private_ip]
- # records        = [aws_instance.instance[each.value]["name"].private_ip]
+#  records        = [aws_instance.instance[each.value].private_ip]
+  records        = [aws_instance.instance[each.value["name"]].private_ip]
 }
