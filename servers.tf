90e1dcfde15262bd68d6a4be5a2fccd5f4cf343d
mammu <bandreddymammu599@gmail.com>
2025-02-16 20:07:31 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -12,7 +12,7 @@ resource "aws_instance" "instance" {
   for_each               = var.components
   ami                    = data.aws_ami.centos.image_id
   instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-1 .id]
 
   tags = {
     Name = each.value["name"]
@@ -22,24 +22,24 @@ resource "aws_instance" "instance" {
 # resource "null_resource" "provisioner" {
 #
 #   depends_on = [aws_instance.instance, aws_route53_record.records]
-#   provisioner "remote-exec" {
-#
-#     connection {
-#       type     = "ssh"
-#       user     = "root"
-#       password = "DevOps321"
-#       host     = aws_instance.instance.private_ip
-#     }
-#
-#     inline = [
-#       "rm -rf roboshop-shell",
-#       "git clone https://github.com/Madhuri-Bandreddi/roboshop-shell.git",
-#       "cd roboshop-shell",
-#       "sudo bash ${var.component_name}.sh.${var.password}"
-#     ]
-#   }
-# }
-#
+  provisioner "remote-exec" {
+
+    connection {
+      type     = "ssh"
+      user     = "root"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+
+    inline = [
+      "rm -rf roboshop-shell",
+      "git clone https://github.com/Madhuri-Bandreddi/roboshop-shell.git",
+      "cd roboshop-shell",
+      "sudo bash ${each.value["name"]}.sh"
+     ]
+   }
+ }
+
 resource "aws_route53_record" "records" {
   for_each       = var.components
   zone_id        = "Z06377673P2QZ3HGG0TOY"
