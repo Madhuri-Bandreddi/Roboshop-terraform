d173d60d2c42e055644a1a16c9226f7d23b40246
mammu <bandreddymammu599@gmail.com>
2025-02-16 18:51:16 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -62,7 +62,6 @@ resource "aws_instance" "instance" {
   ami            = data.aws_ami.centos.image_id
   instance_type =each.value["instance_type"]
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-16.id]
-
   tags = {
     Name = each.value["name"]
   }
