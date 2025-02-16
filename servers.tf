a856668f71590b675f68147a5cc30841c7401ec2
mammu <bandreddymammu599@gmail.com>
2025-02-16 18:38:44 +0530

    updating ./Roboshop-terraform

--- servers.tf
+++ servers.tf
@@ -1,73 +1,73 @@
-module "servers" {
-  for_each = var.components
+# module "servers" {
+#   for_each = var.components
+#
+#   source         = "./module"
+#   component_name = each.value["name"]
+#   env            = var.env
+#   instance_type  = each.value["instance_type"]
+#   password       = lookup(each.value, "password", "null")
+# }
+variable "components" {
+  default = {
+    frontend = {
+      name          = "frontend"
+      instance_type = "t2.micro"
+    }
+    mongodb = {
+      name          = "mongodb"
+      instance_type = "t2.micro"
+    }
+    catalogue = {
+      name          = "catalogue"
+      instance_type = "t3.medium"
+    }
+    redis = {
+      name          = "redis"
+      instance_type = "t3.micro"
+    }
+    user = {
+      name          = "user"
+      instance_type = "t3.micro"
+    }
+    cart = {
+      name          = "cart"
+      instance_type = "t3.micro"
+    }
+    mysql = {
+      name          = "mysql"
+      instance_type = "t3.small"
+      password      = "RoboShop@1"
+    }
+    shipping = {
+      name          = "shipping"
+      instance_type = "t3.medium"
+      password      = "RoboShop@1"
+    }
+    rabbitmq = {
+      name          = "rabbitmq"
+      instance_type = "t2.micro"
+      password      = "roboshop123"
 
-  source         = "./module"
-  component_name = each.value["name"]
-  env            = var.env
-  instance_type  = each.value["instance_type"]
-  password       = lookup(each.value, "password", "null")
+    }
+    payment = {
+      name          = "payment"
+      instance_type = "t3.micro"
+      password      = "roboshop123"
+    }
+  }
 }
-# variable "components" {
-#   default = {
-#     frontend = {
-#       name          = "frontend"
-#       instance_type = "t2.micro"
-#     }
-#     mongodb = {
-#       name          = "mongodb"
-#       instance_type = "t2.micro"
-#     }
-#     catalogue = {
-#       name          = "catalogue"
-#       instance_type = "t3.medium"
-#     }
-#     redis = {
-#       name          = "redis"
-#       instance_type = "t3.micro"
-#     }
-#     user = {
-#       name          = "user"
-#       instance_type = "t3.micro"
-#     }
-#     cart = {
-#       name          = "cart"
-#       instance_type = "t3.micro"
-#     }
-#     mysql = {
-#       name          = "mysql"
-#       instance_type = "t3.small"
-#       password      = "RoboShop@1"
-#     }
-#     shipping = {
-#       name          = "shipping"
-#       instance_type = "t3.medium"
-#       password      = "RoboShop@1"
-#     }
-#     rabbitmq = {
-#       name          = "rabbitmq"
-#       instance_type = "t2.micro"
-#       password      = "roboshop123"
-#
-#     }
-#     payment = {
-#       name          = "payment"
-#       instance_type = "t3.micro"
-#       password      = "roboshop123"
-#     }
-#   }
-# }
 
-# resource "aws_instance" "instance" {
-#   for_each       = var.components
-#   ami           = data.aws_ami.centos.image_id
-#   instance_type =each.value["instance_type"]
-#
-#   vpc_security_group_ids = [data.aws_security_group.launch-wizard-16.id]
-#
-#   tags = {
-#     Name = each.value["name"]
-#   }
-# }
+resource "aws_instance" "instance" {
+  for_each       = var.components
+  ami           = data.aws_ami.centos.image_id
+  instance_type =each.value["instance_type"]
+
+  vpc_security_group_ids = [data.aws_security_group.launch-wizard-16.id]
+
+  tags = {
+    Name = each.value["name"]
+  }
+}
 #
 # resource "null_resource" "provisioner" {
 #
@@ -90,11 +90,11 @@ module "servers" {
 #   }
 # }
 #
-# resource "aws_route53_record" "records" {
-#   for_each       = var.components
-#   zone_id        = "Z06377673P2QZ3HGG0TOY"
-#   name           = "${each.value["name"]}.madhari123.shop"
-#   type           = "A"
-#   ttl            = 30
-#   records        = [aws_instance.instance[each.value["name"]].private_ip]
-# }
+resource "aws_route53_record" "records" {
+  for_each       = var.components
+  zone_id        = "Z06377673P2QZ3HGG0TOY"
+  name           = "${each.value["name"]}.madhari123.shop"
+  type           = "A"
+  ttl            = 30
+  records        = [aws_instance.instance[each.value["name"]].private_ip]
+}
