dc612d5c81f2a468b4db7a1048c38fa4b5deef6d
mammu <bandreddymammu599@gmail.com>
2025-02-16 18:42:10 +0530

    terraform

--- servers.tf
+++ servers.tf
@@ -59,9 +59,8 @@ variable "components" {
 
 resource "aws_instance" "instance" {
   for_each       = var.components
-  ami           = data.aws_ami.centos.image_id
+  ami            = data.aws_ami.centos.image_id
   instance_type =each.value["instance_type"]
-
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-16.id]
 
   tags = {
