f790ac8affb638d512a6c7c5dd0f0e3ed17d7aac
mammu <bandreddymammu599@gmail.com>
2025-02-23 00:45:40 +0530

    updating ./Roboshop-terraform

--- module/main.tf
+++ module/main.tf
@@ -2,14 +2,13 @@ resource "aws_instance" "instance" {
   ami                    = data.aws_ami.centos.image_id
   instance_type          = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.launch-wizard-1.id]
-
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
   tags = {
     Name = local.name
   }
 }
 
 resource "null_resource" "provisioner" {
-  count = var.provisioner ? 1 : 0
   depends_on = [aws_instance.instance, aws_route53_record.records]
   provisioner "remote-exec" {
 
@@ -20,12 +19,7 @@ resource "null_resource" "provisioner" {
       host     = aws_instance.instance.private_ip
     }
 
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/Madhuri-Bandreddi/roboshop-shell.git",
-      "cd roboshop-shell",
-      "sudo bash ${var.component_name}.sh ${var.password}"
-    ]
+    inline = var.app_type == "db" ? local.db_commands : local. app_commands
   }
 }
 
@@ -36,4 +30,54 @@ resource "aws_route53_record" "records" {
   type     = "A"
   ttl      = 30
   records  = [aws_instance.instance.private_ip]
+}
+
+resource "aws_iam_role" "role" {
+  name = "${var.component_name}-${var.env}-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2. amazonaws. com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "$(var.component_name}-${var.env}-role"
+  }
+}
+
+resource "aws_iam_role_policy" "ssm-ps-policy" {
+  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+  role = aws_iam_role.role.id
+
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "VisualEditor0",
+        "Effect" : "Allow",
+        "Action" : [
+          "ssm: GetParameterHistory",
+          "ssm: GetParametersByPath",
+          "ssm: GetParameters",
+          "ssm: GetParameter"
+        ],
+        "Resource" : "arn:aws:ssm:us-east-1:633788536644:parameter/${var.env}.${var.component_name].*"
+      },
+      {
+        "Sid" : "VisualEditor1",
+        "Effect" : "Allow",
+        "Action" : "ssm:DescribeParameters",
+        "Resource" : "*"
+      }
+    ]
+  })
 }
