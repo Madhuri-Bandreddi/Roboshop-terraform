ac358c3d2740116991f2408764441a955a9318f0
mammu <bandreddymammu599@gmail.com>
2025-02-28 11:36:36 +0530

    updating ./Roboshop-terraform

--- module/main.tf
+++ module/main.tf
@@ -59,7 +59,7 @@ resource "aws_iam_role" "role" {
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "${var.component_name}-${var.env}-role"
-  role = aws_iam_role.role.id
+  role = aws_iam_role.role.name
 }
 resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
