7a6f22fe39d0cfbd02d92e21fa8d9ec762c8e687
mammu <bandreddymammu599@gmail.com>
2025-03-02 15:40:22 +0530

    terraform

--- module/locals.tf
+++ module/locals.tf
@@ -2,7 +2,7 @@ locals {
   name = var.env != "" ? "${var.component_name}-${var.env}" : var.component_name
   db_commands = [
     "rm -rf roboshop-shell",
-    "git clone https://github.com/Madhuri-Bandreddi/roboshop-shell.git",
+    "git clone https://github.com/Madhuri-Bandreddi/roboshop-shell",
     "cd roboshop-shell",
     "sudo bash ${var.component_name}.sh ${var.password}"
   ]
